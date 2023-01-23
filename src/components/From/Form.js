cb98624e54dc9f068fac95223f9697e9b00cfdc5
Amdrii <andreyk557@gmail.com>
2023-01-23 21:56:34 +0200

    feat[new/part]

---
+++ src/components/From/Form.js
@@ -0,0 +1,68 @@
+// IMPORTS
+
+import React, { useState, useEffect } from 'react';
+import { Card } from '../Card/Card';
+import { Repos } from '../Repos/Repos';
+import { Error } from '../Error/Error';
+import s from './Form.module.scss';
+
+// IMPORTS
+
+
+const GITHUB_API_URL = 'https://api.github.com/users/';
+const USER_NOT_FOUND_MESSAGE = 'No user with this name. Please try anuther name.';
+const LAST_REPOS_QUANTITY = 10;
+
+export function Form() {
+  const [text, setText] = useState('');
+  const [userInfo, setUserInfo] = useState(null);
+  const [userRepos, setUserRepos] = useState(null);
+  const [isError, setIsError] = useState(false);
+
+  const handleChange = (e) => {
+    setIsError(false);
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`${GITHUB_API_URL}${text}`)
+      .then((res) => {
+        if (res.ok) {
+          setIsError(false);
+          return res.json();
+        }
+        throw new Error(`There are no user ${text}`);
+      })
+      .then((json) => setUserInfo(json))
+      .catch(() => setIsError(true));
+  };
+
+  useEffect(() => {
+    userInfo &&
+      fetch(userInfo.repos_url)
+        .then((res) => res.json())
+        .then((json) => setUserRepos(json));
+  }, [userInfo]);
+
+  return (
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <h2 className={s.title}>Search <span className='titel-sub-word'>GitHub</span> user</h2>
+          <input
+            className={s.input}
+            value={text}
+            onChange={handleChange}
+            placeholder="Search github profile name"
+          />
+          {isError && <Error errorMessage={USER_NOT_FOUND_MESSAGE} />}
+        </form>
+      </div>
+      {userInfo && <Card userInfo={userInfo} />}
+      {userRepos && (
+        <Repos userRepos={userRepos} reposQuantity={LAST_REPOS_QUANTITY} />
+      )}
+    </>
+  );
+}
